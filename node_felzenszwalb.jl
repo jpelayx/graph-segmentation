23217475175f249b5052634aad92be4907084078
Julia Rodrigues <j.pelayo.rodrigues@gmail.com>
2025-02-19 16:52:34 -0300

    Revert "wip: backward with gpu"
    This reverts commit b334af14f164263d8212ecc6e6ced2f718e9fa1d.

--- node_felzenszwalb.jl
+++ node_felzenszwalb.jl
@@ -11,7 +11,6 @@ using LinearAlgebra
 using SparseArrays
 using DataStructures: Stack, pop!, push! 
 using CUDA
-using CUDA.CUSPARSE
 using Flux
 using Adapt: @adapt_structure
 
@@ -38,9 +37,9 @@ Segmentation(N::Int) = Segmentation(Matrix{Float64}(I,N,N), zeros(N), ones(N))
 
 struct FelzenszwalbStep 
     t::Int
-    ΔS
-    ΔInt
-    Δsize
+    ΔS::SparseMatrixCSC{Float64, Int}
+    ΔInt::SparseVector{Float64, Int}
+    Δsize::SparseVector{Float64, Int}
 end 
 
 struct FelzenszwalbTape 
@@ -55,14 +54,12 @@ function Base.isempty(tape::FelzenszwalbTape)
 end
 
 function record!(tape::FelzenszwalbTape, ΔS, ΔInt, Δsize, t)
-    @CUDA.allowscalar ΔInt = sparsevec(ΔInt)
+    ΔInt = sparsevec(ΔInt)
     if isempty(ΔInt.nzval)
         return 
     end 
-    @CUDA.allowscalar Δsize = sparsevec(Δsize) 
-    ΔInt = CuSparseVector(ΔInt)
-    Δsize = CuSparseVector(Δsize)
-    ΔS = CuSparseMatrixCSR(ΔS)
+    Δsize = sparsevec(Δsize) 
+    ΔS = sparse(ΔS)
     push!(tape.stack, FelzenszwalbStep(t, ΔS, ΔInt, Δsize))
 end
 
@@ -85,9 +82,9 @@ end
 function clear_intersections!(P, Vi, Ui)
     intersections = Vi ∩ Ui
     if !isempty(intersections)
-        is = something.(indexin(intersections, Vi))
-        js = something.(indexin(intersections, Ui))
-        CUDA.@allowscalar P[js, is] .= 0.0
+        is = indexin(intersections, Vi)
+        js = indexin(intersections, Ui)
+        P[js, is] .= 0.0
     end
     return P
 end
@@ -106,24 +103,28 @@ function ChainRulesCore.rrule(::typeof(clear_intersections!), P, Vi, Ui)
     return clear_intersections!(P, Vi, Ui), pullback
 end
 
-function merge_probability(S, internal_diff, segment_size, v, u, weight, k, μ, ϵ=1e-6, auxmem=nothing)
-    ignore_derivatives() do 
-        P = auxmem.P
-        V = auxmem.V
-        U = auxmem.U
-        condition = auxmem.τ
-    end 
-    
-    condition = internal_diff .+ (k ./ segment_size)
-    V = - condition * auxmem.N1'
-    U = - auxmem.N1 * condition'
-    
-    P = (V .+ U .+ sqrt.((V .- U) .^ 2 .+ ϵ)) ./ 2
-    P = tanh.((P .- weight) .* μ)
-    P = P .* (S[u,:] * S[v,:]')
-      
-    return P
+
+function merge_probability(
+    Vi, V, Ui, U,
+    internal_diff,
+    segment_size,
+    v, u, weight
+)
+    τ(Vi, k) = k ./ segment_size[Vi]
+    MInt = minimum.(
+        Iterators.product(
+            internal_diff[Ui] .+ τ(Ui, k),
+            internal_diff[Vi] .+ τ(Vi, k)
+        )
+    )
+    Mij_conditional = tanh.((MInt .- weight) .* μ)
+
+    clear_intersections!(Mij_conditional, Vi, Ui)
+
+    Mij = relu.(Mij_conditional .* (U[u,:] * V[v,:]'))
+    return Mij
 end
+
 
 function adjust_u!(dU, U, i) 
     dU[i, :] .*= U[i, :]
@@ -177,7 +178,7 @@ end
 
 function make_dS(dV, Vi, dU, Ui)
     N = size(dV)[1]
-    dS = zeros(N,N) |> cu
+    dS = zeros(N,N)
     dS[:, Vi] .= dV
     dS[:, Ui] .+= dU
     return dS
@@ -209,40 +210,41 @@ end
 
 
 function f(
-    S, 
-    internal_diff, 
-    segment_size,
+    S::Matrix{Float64}, 
+    internal_diff::Vector{Float64}, 
+    segment_size::Vector{Float64},
     t, w, E,
     tape=nothing
-    auxmem=nothing
 )
     weight = w[t]
     v, u = E[t]
+    Vi = findall(x -> x > 0.0, S[v, :])
+    Ui = findall(x -> x > 0.0, S[u, :])
+    V = @view S[:, Vi]
+    U = @view S[:, Ui]
 
-    P = merge_probability(S, internal_diff, segment_size, v, u, weight, k, μ, auxmem)
+    P = merge_probability(Vi, V, Ui, U, internal_diff, segment_size, v, u, weight)
     
-    U = S * Diagonal(vec(sum(P, dims=2)))
-    adjust_u!(U, S, u)
+    dU = U * Diagonal(vec(sum(P, dims=2)))
+    adjust_u!(dU, U, u)
 
-    V = (1 .- S) .* (S * P)
-    adjust_v!(V, v)
+    dV = (1 .- V) .* (U * P)
+    adjust_v!(dV, v)
 
-    dS = U .+ V
+    dS = make_dS(dV, Vi, -dU, Ui)
 
     Mi = sum(P, dims=1)'
-    internal_diff_offset = (1 .- Mi) .* internal_diff .+ Mi .* weight - internal_diff
-    # internal_diff_offset = zeros(size(S)[1]) |> cu 
-    # fillvec!(internal_diff_offset, 
-    #     Vi, 
-    #     ((1 .- Mi) .* internal_diff[Vi] .+ Mi .* weight) - internal_diff[Vi]
-    # )
+    internal_diff_offset = zeros(size(S)[1])
+    fillvec!(internal_diff_offset, 
+        Vi, 
+        ((1 .- Mi) .* internal_diff[Vi] .+ Mi .* weight) - internal_diff[Vi]
+    )
 
-    segment_size_offset = [sum(col .* segment_size) for col in eachcol(P)] |> cu
-    # segment_size_offset = zeros(size(S)[1]) |> cu
-    # fillvec!(segment_size_offset, 
-    #     Vi, 
-    #     [sum(col .* segment_size[Ui]) for col in eachcol(P)] |> cu
-    # )
+    segment_size_offset = zeros(size(S)[1])
+    fillvec!(segment_size_offset, 
+        Vi, 
+        [sum(col .* segment_size[Ui]) for col in eachcol(P)]
+    )
     
     @ignore_derivatives if tape !== nothing
         record!(tape, dS, internal_diff_offset, segment_size_offset, t)
@@ -251,45 +253,25 @@ function f(
     return dS, internal_diff_offset, segment_size_offset
 end
 
-global P = CuArray{Float64}(undef, N, N)
-global V = CuArray{Float64}(undef, N, N)
-global U = CuArray{Float64}(undef, N, N)
-global NOnes = CUDA.ones(Float64, N)
-global tau = CuArray{Float64}(undef, N)
-
-
-
 function felzenszwalb_solve(G::GNNGraph)
     src, dst = edge_index(G)
     
     w = mean(sqrt.((G.x[:, src] .- G.x[:, dst]) .^ 2), dims=1)
     edge_order = sortperm(w, dims=2)
-    w = w[edge_order] |> cpu 
+    w = w[edge_order]
 
     src, dst = src[edge_order], dst[edge_order]    
     E = collect(zip(src, dst))
 
     N = G.num_nodes
-    S = CuArray{Float64}(I,N,N) 
-    internal_diff = CUDA.zeros(N) 
-    segment_size = CUDA.ones(N) 
+    S = Matrix{Float64}(I,N,N)
+    internal_diff = zeros(N)
+    segment_size = ones(N)
 
-    # allocating memory for ops 
-    auxmem = (
-        dS = CuArray{Float64}(undef, N, N),
-        internal_diff_offset = CuArray{Float64}(undef, N),
-        segment_size_offset = CuArray{Float64}(undef, N),
-        P = CuArray{Float64}(undef, N, N),
-        V = CuArray{Float64}(undef, N, N),
-        U = CuArray{Float64}(undef, N, N),
-        N1 = CUDA.ones(Float64, N),
-        τ = CuArray{Float64}(undef, N)
-    )
-
-    tape = FelzenszwalbTape() |> cu
+    tape = FelzenszwalbTape()
 
     for t in 1:length(E)
-        dS, internal_diff_offset, segment_size_offset = f(S, internal_diff, segment_size, t, w, E, tape, auxmem)
+        dS, internal_diff_offset, segment_size_offset = f(S, internal_diff, segment_size, t, w, E, tape)
         S += dS
         internal_diff += internal_diff_offset
         segment_size += segment_size_offset
@@ -313,7 +295,7 @@ function felzenszwalb_reverse(
     w = mean(sqrt.((G.x[:, src] .- G.x[:, dst]) .^ 2), dims=1)
     edge_order = sortperm(w, dims=2)
     w = w[edge_order]
-    Δw = zeros(size(w)) 
+    Δw = zeros(size(w)) |> cu
 
     src, dst = src[edge_order], dst[edge_order]    
     E = collect(zip(src, dst))
@@ -331,6 +313,7 @@ function felzenszwalb_reverse(
         ∇size .+= -δsize
         ∇ = (∇S, ∇Int, nothing)
     end
+
     return Δw
 end
 
@@ -340,11 +323,8 @@ function compute_edge_weights(x, edge_index)
 end
 
 G = G |> cu
-S, internal_diff, segment_size, tape = felzenszwalb_solve(G);
-S = S  |> cu
-internal_diff = internal_diff |> cu
-segment_size = segment_size |> cu
-tape = tape |> cu
+S, internal_diff, segment_size, tape = felzenszwalb_solve(G)
 N, _ = size(S)
-∇ = (rand(N,N) |> cu, rand(N) |> cu, rand(N) |> cu)
+∇ = (rand(N,N), rand(N), rand(N)) |> cu
+# ∇ = (rand(N,N), rand(N), nothing) 
 Δw = felzenszwalb_reverse(G, S, internal_diff, segment_size, tape, ∇)
